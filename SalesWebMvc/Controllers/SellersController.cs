c70653d222da873b19161d5cb10b054ef6079ac8
Camily Santos <camilycsantos@gmail.com>
2023-12-28 23:37:44 -0300

    Simple Create form

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -7,7 +7,7 @@ namespace SalesWebMvc.Controllers
     public class SellersController : Controller
     {
         private readonly SellerService _sellerService;
-        
+
         public SellersController(SellerService sellerService)
         {
             _sellerService = sellerService;
